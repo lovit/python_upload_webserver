caa710a2bb8346ff7154a7bc5f292980dc0714c1
Hyunjoong Kim <lovit@users.noreply.github.com>
2020-10-08 15:35:49 +0900

    Describe files in upload folder

--- python_uploader/app.py
+++ python_uploader/app.py
@@ -8,6 +8,38 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 
+HTML_TEMPLATE = """
+<!doctype html>
+<title>Upload new File</title>
+<h1>Upload new File</h1>
+<form action="" method=post enctype=multipart/form-data>
+  <p><input type=file name=file>
+     <input type=submit value=Upload>
+</form>
+<h2>File list at {}</h2>
+<p>{}</p>"""
+
+def listdir(root):
+    def get_humanize_size(name):
+        path = os.path.join(root, name)
+        s = os.stat(path).st_size
+        if s < 1024:
+            return f"{s} byte"
+        if s < 1048576:
+            return f"{int(s / 1024)} Kb"
+        if s < 1073741824:
+            return f"{int(s / 1048576)} Mb"
+        return f"{int(s / 1073741824)} Gb"
+
+    ROW_FORMAT = "<tr>{}</tr>".format(''.join(["<th>{}</th>"] * 3))
+    filenames = sorted([name for name in os.listdir(root) if os.path.isfile(f'{root}/{name}')])
+    dirnames = sorted([name for name in os.listdir(root) if os.path.isdir(f'{root}/{name}')])
+
+    rows = [ROW_FORMAT.format('DIR', name, ' - ') for name in dirnames]
+    rows += [ROW_FORMAT.format('FILE', name, get_humanize_size(name)) for name in filenames]
+    return '\n'.join(rows)
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -16,16 +48,9 @@ def index():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('index'))
-    return """
-    <!doctype html>
-    <title>Upload a file</title>
-    <h1>Upload a file</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    <p>%s</p>
-    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+    UPLOAD_FOLDER = app.config['UPLOAD_FOLDER']
+    HTML = HTML_TEMPLATE.format(UPLOAD_FOLDER, listdir(UPLOAD_FOLDER))
+    return HTML
 
 
 def main():
